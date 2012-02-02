925d7ccf64b482763dd5b405aa4756161a2f7bb0
Rudie Dirkx <github@hotblocks.nl>
2012-02-02 20:45:46 +0100

    nocrop BUG

--- browse.php
+++ browse.php
@@ -5,7 +5,6 @@ require_once('inc.table.php');
 $iPage = isset($_GET['page']) && 0 <= (int)$_GET['page'] ? (int)$_GET['page'] : 0;
 $iLimit = 200;
 $iStart = $iPage * $iLimit;
-$bCrop = empty($_GET['nocrop']);
 
 $szSql = 'SELECT * FROM "'.$szTable.'" WHERE 1 LIMIT '.$iStart.', '.$iLimit;
 if ( !empty($_GET['sql']) ) {
@@ -14,7 +13,10 @@ if ( !empty($_GET['sql']) ) {
 
 //$arrTable = $db->structure($szTable);
 
-echo '<form action="?">';
+$nocrop = (int)!empty($_GET['nocrop']);
+
+echo '<form action="">';
+echo '<input type="hidden" name="nocrop" value="'.$nocrop.'" />';
 echo '<input type="hidden" name="db" value="'.$_GET['db'].'" />';
 echo '<input type="hidden" name="tbl" value="'.$_GET['tbl'].'" />';
 echo '<textarea id="sqlq" name="sql" style="width:99%;" rows=4 autofocus>'.htmlspecialchars($szSql).'</textarea>';
@@ -25,7 +27,9 @@ $arrContents = $db->fetch($szSql);
 if ( $arrContents ) {
 	echo '<style style="text/css">tbody.pre td { font-family:\'Courier New\'; font-size:13px; white-space:pre; }</style>';
 	echo '<table border="1" cellpadding="4" cellspacing="2">';
-	echo '<thead><tr><th colspan="'.count($arrContents[0]).'">'.count($arrContents).' / '.$db->count($szTable, '1').' records | <a href="?'.$_SERVER['QUERY_STRING'].'&nocrop=1">nocrop</a></th></tr>';
+	$_GET['nocrop'] = (int)!$nocrop;
+	$qs = http_build_query($_GET);
+	echo '<thead><tr><th colspan="'.count($arrContents[0]).'">'.count($arrContents).' / '.$db->count($szTable, '1').' records | <a href="?'.$qs.'">'.( $nocrop ? 'crop' : 'nocrop' ).'</a></th></tr>';
 	echo '<tr>';
 	foreach ( $arrContents[0] AS $k => $v ) {
 		echo '<th>'.$k.'</th>';
@@ -35,7 +39,7 @@ if ( $arrContents ) {
 	foreach ( $arrContents AS $k => $r ) {
 		echo '<tr>';
 		foreach ( $r AS $k => $v ) {
-			echo '<td>'.( null === $v ? '<i>NULL</i>' : ( $bCrop && 50 < strlen($v) ? htmlspecialchars(substr($v, 0, 47)).'...' : htmlspecialchars($v) ) ).'</td>';
+			echo '<td>'.( null === $v ? '<i>NULL</i>' : ( !$nocrop && 80 < strlen($v) ? htmlspecialchars(substr($v, 0, 78)).'...' : htmlspecialchars($v) ) ).'</td>';
 		}
 		echo '</tr>';
 	}
