1fc30a9a12b6c9fba509987b0a6b75bca7da4a15
Rudie Dirkx <github@hotblocks.nl>
2016-11-04 19:01:06 +0100

    +favicon

--- tpl.header.php
+++ tpl.header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="utf-8" />
 	<link rel="stylesheet" href="base.css" />
+	<link rel="icon" href="favicon.png" />
 	<title>SQLite Admin</title>
 	<style>
 	input, select, textarea {
